948510f352140380168bc14e45aa28806a19cb8c
Justin <justin27sackprasit@gmail.com>
2022-06-23 19:13:36 +1000

    Stylise nav buttons

--- src/index.js
+++ src/index.js
@@ -15,10 +15,14 @@ function createHeader() {
   
   const headerNav = document.createElement('nav');
   const homeButton = document.createElement('button');
+  homeButton.classList.add('nav-button');
   homeButton.textContent = 'Home';
+  setActiveButton(homeButton);
   const menuButton = document.createElement('button');
+  menuButton.classList.add('nav-button');
   menuButton.textContent = 'Menu';
   const contactButton = document.createElement('button');
+  contactButton.classList.add('nav-button');
   contactButton.textContent = 'Contact';
   
   addListeners(homeButton, menuButton, contactButton);
@@ -34,15 +38,18 @@ function createHeader() {
 function addListeners(home, menu, contact) {
   home.addEventListener('click', () => {
     clearMainContent();
+    setActiveButton(home);
     checkMenuFlex();
     loadInitialPage();
   });
   menu.addEventListener('click', () => {
     clearMainContent();
+    setActiveButton(menu);
     loadMenuPage();
   });
   contact.addEventListener('click', () => {
     clearMainContent();
+    setActiveButton(contact);
     checkMenuFlex();
     loadContactPage();
   });
@@ -61,6 +68,16 @@ function clearMainContent() {
     mainContent.removeChild(mainContentChild);
     mainContentChild = mainContent.lastElementChild;
   }
+}
+
+function setActiveButton(button) {
+  const btns = document.querySelectorAll('.nav-button');
+  btns.forEach(btn => {
+    if (btn.classList.contains('active')) {
+      btn.classList.remove('active');
+    }
+  });
+  button.classList.add('active');
 }
 
 function checkMenuFlex() {
